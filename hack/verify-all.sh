af90fc60100ab7986f240fa37efc083d574eb740
Di Xu <stephenhsu90@gmail.com>
2023-04-11 20:19:40 +0800

    bump kubernetes dependencies to 1.26.3

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -8,6 +8,7 @@ SCRIPT_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
 make -C "${SCRIPT_ROOT}" tidy
 STATUS=$(cd "${SCRIPT_ROOT}" && git status --porcelain go.mod go.sum)
 if [ ! -z "$STATUS" ]; then
+  git diff --color go.mod go.sum | cat
   echo "Running 'go mod tidy' to fix your 'go.mod' and/or 'go.sum'"
   exit 1
 fi
